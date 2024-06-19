8f308db536849958aa123a43268882a621dc7687
Matheus Boy <mboy@redhat.com>
2024-06-19 18:39:02 -0300

    better comments

--- tour/variables/variables.go
+++ tour/variables/variables.go
@@ -8,12 +8,13 @@ var c, python, java bool
 var j, k int = 1, 2
 
 func main() {
+	// differently than C, looks like golang has default init values for variables
 	var i int
 	// type is defined during initialization
 	var c_init, python_init, java_init = true, false, "no!"
+	// only available inside functions
 	my_var := 3
 	// 0 false false false
-	// differently than C, looks like golang has default init values for variables
 	fmt.Println(i, c, python, java)
 	// displays the initialized values
 	fmt.Println(j, k, c_init, python_init, java_init)

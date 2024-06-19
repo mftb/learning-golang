8bf395632be31203cb79e93a06e25d19869940cb
Matheus Boy <mboy@redhat.com>
2024-06-19 15:41:46 -0300

    adding a few more examples

---
+++ tour/functions/functions.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func rec_add(x int, y int) int {
+	if y > 0 {
+		return 1 + rec_add(x, y-1)
+	} else {
+		return x
+	}
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+	fmt.Println(rec_add(42, 13))
+}

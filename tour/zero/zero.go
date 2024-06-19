e4518da0529d579e1d6235a608d334b8d1fb75ee
Matheus Boy <mboy@redhat.com>
2024-06-19 18:48:20 -0300

    a study on default values

---
+++ tour/zero/zero.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int     // default 0
+	var f float64 //default 0
+	var b bool    // default false
+	var s string  // default ""
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
